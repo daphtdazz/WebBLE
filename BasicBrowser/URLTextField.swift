f696f4ca51868bbfed79c88b6c34c283213bee76
daphtdazz <daphtdazz@icloud.com>
2026-09-15 09:48:07 +0100

    Fix occasional crash due to invalidated constraint re-use in URLTextField

--- BasicBrowser/URLTextField.swift
+++ BasicBrowser/URLTextField.swift
@@ -25,24 +25,18 @@ class URLTextField: UITextField {
         return answer
     }
 
+    override func willMove(toSuperview newSuperview: UIView?) {
+        NSLayoutConstraint.deactivate(self.superViewConstraints)
+        self.superViewConstraints = []
+    }
+
     override func didMoveToSuperview() {
-        if self.superViewConstraints.count > 0 {
-            NSLayoutConstraint.deactivate(self.superViewConstraints)
-            // don't necessarily delete them:
-            // we may be able to reuse them
-        }
-
-        if let sv = self.superview {
-            if self.superViewConstraints.count == 0
-                || self.superViewConstraints[0].secondItem as? UIView != sv {
-                // need new constraints
-                self.superViewConstraints = [
-                    self.leadingAnchor.constraint(equalTo: sv.leadingAnchor, constant: 8),
-
-                    self.trailingAnchor.constraint(equalTo: sv.trailingAnchor, constant: -8),
-                ]
-            }
-            NSLayoutConstraint.activate(self.superViewConstraints)
-        }
+        guard let sv = self.superview else { return }
+        let leading = self.leadingAnchor.constraint(equalTo: sv.leadingAnchor, constant: 8)
+        leading.identifier = "URLTextField.leading"
+        let trailing = self.trailingAnchor.constraint(equalTo: sv.trailingAnchor, constant: -8)
+        trailing.identifier = "URLTextField.trailing"
+        self.superViewConstraints = [leading, trailing]
+        NSLayoutConstraint.activate(self.superViewConstraints)
     }
 }
